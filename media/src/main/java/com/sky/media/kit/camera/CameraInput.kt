dd210652387422b500edae2b5c6cfad09487e844
xuzhiyong <18971269648@163.com>
2021-08-16 14:04:33 +0800

    fps

--- media/src/main/java/com/sky/media/kit/camera/CameraInput.kt
+++ media/src/main/java/com/sky/media/kit/camera/CameraInput.kt
@@ -97,6 +97,7 @@ class CameraInput(val iRenderView : IRenderView,val mCamera: Camera) : TextureOu
             e.printStackTrace()
         }
         super.drawFrame()
+        FpsTest.getInstance().countFps()
 
     }
 
